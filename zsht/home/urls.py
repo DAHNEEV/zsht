87a374937ff1196c117a80acc5ee9a8d3e4d2217
DAHNEV <60144273+DAHNEEV@users.noreply.github.com>
2022-06-09 11:19:00 +0200

    pag

--- zsht/home/urls.py
+++ zsht/home/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from .views import ArticleDetailView
+from .views import ArticleListView
 
 urlpatterns = [
-    path('', views.home, name="zsht-home"),
+    path('', ArticleListView.as_view(), name="zsht-home"),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name="zsht-article"),
 ]
--- zsht/home/views.py
+++ zsht/home/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Article
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-def home(request):
-    articles = { 'articles': Article.objects.all().order_by('-id') }
-    return render(request, 'home/home.html', articles)
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'home/home.html'
+    ordering = ['-id']
+    paginate_by = 5
 
 class ArticleDetailView(DetailView):
     model = Article
